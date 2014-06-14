0fe469c6cdce933d0facb94d7f971bfbb8cab30e
tpillairavin <ravin@gocardless.com>
2014-06-14 11:40:08 +0100

    First Commit -General Assembly

---
+++ goals.rb
@@ -0,0 +1,6 @@
+#My goals for this course are:
+puts "hello world!"
+puts "Alina <3 Ruby"
+myname = "Ravin"
+mystatus = "Champion"
+puts "My goal for this course is to detail the status of #{myname} as a #{mystatus}"
