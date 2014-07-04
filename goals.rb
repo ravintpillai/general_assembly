17c7a7ab3a71fcd2b5b479e603a63eb1d59a0f38
tpillairavin <ravin@gocardless.com>
2014-07-04 02:16:01 +0100

    remove nonsense goals file

--- goals.rb
+++
@@ -1,9 +0,0 @@
-#My goals for this course are:
-puts "hello world!"
-puts "Alina <3 Ruby"
-myname = "Ravin"
-mystatus = "Champion"
-puts "My goal for this course is to detail the status of #{myname} as a #{mystatus}"
-puts "hey"
-puts "yo wassup"
-puts "hows shizzle?"
